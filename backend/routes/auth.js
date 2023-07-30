31b2af8709f8cdd5630809cd73c46e11d04d37f4
juanluna96 <juandluna17@gmail.com>
2023-07-29 22:25:13 -0500

    Signup validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,9 +5,14 @@ const express = require("express");
 const router = express.Router();
 
 // import controller methods
-const { auth } = require("../controllers/auth");
+const { signup } = require("../controllers/auth");
+
+const { runValidation } = require("../validators");
+
+// validators
+const { userSignupValidator } = require("../validators/auth");
 
 // routes
-router.post("/auth", auth);
+router.post("/signup", userSignupValidator, runValidation, signup);
 
 module.exports = router;
