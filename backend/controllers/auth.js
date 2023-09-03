23828f153c3226f4d1340b7227548102d333113c
juanluna96 <juandluna17@gmail.com>
2023-09-03 17:24:24 -0500

    Fixing callback mongoose

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,26 +4,28 @@ const shortid = require("shortid");
 exports.signup = (req, res) => {
   const { name, email, password } = req.body;
 
-  User.findOne({ email: email }).exec((error, user) => {
-    if (user) {
-      return res.status(400).json({
-        error: "User already registered",
-      });
-    }
-
-    const username = shortid.generate();
-    const profile = `${process.env.CLIENT_URL}/profile/${username}`;
-
-    const newUser = new User({ name, email, password, profile, username });
-    newUser.save((error, success) => {
-      if (error) {
+  User.findOne({ email: email })
+    .then((user) => {
+      if (user) {
         return res.status(400).json({
-          error: error,
+          error: "User already registered",
         });
       }
+
+      const username = shortid.generate();
+      const profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+      const newUser = new User({ name, email, password, profile, username });
+      return newUser.save();
+    })
+    .then(() => {
       res.json({
         message: "Signup success! Please signin.",
       });
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error: error.message, // Send the error message
+      });
     });
-  });
 };
