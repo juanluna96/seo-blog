35e285147f98f2d7465e1fcb3eeefa1f0cf646c7
juanluna96 <juandluna17@gmail.com>
2023-09-03 17:20:39 -0500

    User signup

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,29 @@
+const User = require("../models/User");
+const shortid = require("shortid");
+
 exports.signup = (req, res) => {
   const { name, email, password } = req.body;
 
-  res.json({
-    user: { name, email, password },
+  User.findOne({ email: email }).exec((error, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "User already registered",
+      });
+    }
+
+    const username = shortid.generate();
+    const profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+    const newUser = new User({ name, email, password, profile, username });
+    newUser.save((error, success) => {
+      if (error) {
+        return res.status(400).json({
+          error: error,
+        });
+      }
+      res.json({
+        message: "Signup success! Please signin.",
+      });
+    });
   });
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
+      trim: true,
       required: true,
-      min: 3,
-      max: 20,
+      max: 32,
       unique: true,
       index: true,
       lowercase: true,
     },
     name: {
       type: String,
+      trim: true,
       required: true,
-      max: 20,
+      max: 32,
     },
     email: {
       type: String,
+      trim: true,
       required: true,
-      max: 50,
       unique: true,
+      lowercase: true,
     },
     profile: {
       type: String,
@@ -30,7 +33,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    salt: Number,
+    salt: String,
     about: {
       type: String,
     },
@@ -43,8 +46,8 @@ const userSchema = new mongoose.Schema(
       contentType: String,
     },
     resetPasswordLink: {
-      data: String,
-      default: "",
+      type: String, // Change the type to String
+      default: "", // Provide a default value as an empty string
     },
   },
   { timestamps: true }
@@ -53,18 +56,17 @@ const userSchema = new mongoose.Schema(
 userSchema
   .virtual("password")
   .set(function (password) {
-    // create a temporary variable called _password
+    // create a temporarity variable called _password
     this._password = password;
     // generate salt
     this.salt = this.makeSalt();
-    // encrypt password
+    // encryptPassword
     this.hashed_password = this.encryptPassword(password);
   })
   .get(function () {
     return this._password;
   });
 
-// methods
 userSchema.methods = {
   authenticate: function (plainText) {
     return this.encryptPassword(plainText) === this.hashed_password;
@@ -74,10 +76,10 @@ userSchema.methods = {
     if (!password) return "";
     try {
       return crypto
-        .createHmac("sha256", this.salt)
+        .createHmac("sha1", this.salt)
         .update(password)
         .digest("hex");
-    } catch (e) {
+    } catch (err) {
       return "";
     }
   },
